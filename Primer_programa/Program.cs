654a4af620bc2164161e8018363b686ecf0830ba
Sebastián <guadalinfo.villanuevadelascruces@guadalinfo.es>
2021-06-09 15:24:49 +0200

    Comprobado

---
+++ Primer_programa/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primer_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); // Para la ejecución hasta presionar la tecla
+        }
+    }
+}
